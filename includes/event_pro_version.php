ea258e10bb875eeb547cc96e9fd58d6adbcd357d
upscalethought <upscalethought@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-22 03:00:35 +0000

    pro page updated
    git-svn-id: https://plugins.svn.wordpress.org/wp-event-booking-manager/trunk@1203623 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/event_pro_version.php
+++ includes/event_pro_version.php
@@ -1,6 +1,6 @@
 <style type="text/css">
   .btnprover {
-      background: linear-gradient(to bottom, #3cb0fd, #3498db) repeat scroll 0 0 #3cb0fd;
+      background: linear-gradient(to bottom, #2638B8, #3498db) repeat scroll 0 0 #3cb0fd;
       border-radius: 3px;
       color: #ffffff;
       font-family: Georgia;
@@ -12,13 +12,13 @@
 <div class="wrapper">
   <div class="wrap" style="float:left; width:100%;">
     <div id="icon-options-general" class="icon32"></div>
-    <div style="width:70%;float:left;"><h2>Wordpress Event Commerce Pro</h2></div>
+    <div style="width:70%;float:left;"><h2>Wordpress Event Booking Manager Pro</h2></div>
        <div class="main_div">
         <div class="metabox-holder" style="width:98%; float:left;">
             <div id="namediv" class="stuffbox" style="width:60%;min-height:450px;">
               <h3 class="top_bar">More Features Available in Our Pro Version(<b>Coming Very Soon</b>):-</h3>
               <div style="padding-top:15px;margin-left:20px;">
-                <a class="btnprover" target="_blank" href="http://wordpress-expert.codeinterest.com" style="width:100px;height: 20px;background-color: green;color:white;" >Wordpress Event Commerce Pro</a>
+                <a class="btnprover" target="_blank" href="http://upscalethought.com/?page_id=9" style="width:100px;height: 20px;background-color: green;color:white;" >Wordpress Event Booking Manager Pro</a>
               </div>  
                <form id="frmCssFix" action="" method="post" style="width:100%">
                  <div style="margin: 20px;">
@@ -42,9 +42,9 @@
                     </ol>
                    <div style="padding-top:15px;">
                      <b>Please Have Our Professional Version to Increase your Business </b><br/><br/><br/>
-                     <a class="btnprover" target="_blank" href="http://wordpress-expert.codeinterest.com" style="width:100px;height: 20px;background-color: green;color:white;" >Wordpress Event Commerce Pro</a>
+                     <a class="btnprover" target="_blank" href="http://upscalethought.com/?page_id=9" style="width:100px;height: 20px;background-color: green;color:white;" >Wordpress Event Booking Manager Pro</a>
                     </div>
-                   <div style="float:right;padding-top:35px; font-size: 11px;">Developed by: <a target="_blank" href="http://www.solvercircle.com">SolverCircle Ltd</a></div>
+                   <div style="float:right;padding-top:35px; font-size: 11px;">Developed by: <a target="_blank" href="http://upscalethought.com/">UpScaleThought Solutions</a></div>
                  </div> 
                </form>	 
             </div>
